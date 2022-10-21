2682c11feeea68a42bc4bc8ce22dd57edfcbe806
domadoes <jemh62korea@gmail.com>
2022-10-21 16:03:11 +0900

    first commit

---
+++ src/main/java/codestates/TodoAppUnit2/TodoAppUnit2Application.java
@@ -0,0 +1,13 @@
+package codestates.TodoAppUnit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppUnit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppUnit2Application.class, args);
+	}
+
+}
